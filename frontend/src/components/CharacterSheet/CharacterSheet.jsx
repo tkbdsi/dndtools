f68e540eed17f1d40264ba2f161b26a9cdf5b3fa
tkbdsi <patient-remix-0q@icloud.com>
2022-04-18 21:32:54 -0400

    feat: added CharacterSheet comp template

---
+++ frontend/src/components/CharacterSheet/CharacterSheet.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+const CharacterSheet = () => {
+  return (
+    <main>
+      <h2>Character Sheet</h2>
+    </main>
+  )
+}
+
+export default CharacterSheet;
